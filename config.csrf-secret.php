2cf44ee77b664e2aaf147eed94c59b8cb8e7495b
Mustafa Omidian <60924400+MustafaOmidian@users.noreply.github.com>
2022-11-16 18:51:07 +0330

    Add files via upload

---
+++ config.csrf-secret.php
@@ -0,0 +1,1 @@
+<?php $secret = "32ca4280c3b8dc32f5e2da3303a6ec192c799e89";
---
+++ parent_tabdata.php
@@ -0,0 +1,13 @@
+<?php
+
+
+//This file contains the commonly used variables 
+
+$parent_tab_info_array=array(1=>'My Home Page',2=>'Marketing',3=>'Sales',4=>'Support',5=>'Analytics',6=>'Inventory',7=>'Tools',8=>'Settings');
+
+
+$parent_child_tab_rel_array=array(1=>array(3,9,),2=>array(4,9,8,),3=>array(4,23,8,9,47,47,),4=>array(4,8,9,),5=>array(1,),6=>array(23,),7=>array(8,40,40,40,50,50,),8=>array(49,49,),);
+
+
+
+?>
